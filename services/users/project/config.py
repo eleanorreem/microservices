1db95b6c953534da422cf0b88222ed07f2db8411
Eleanor Ratliff <eleanorratliff@C01038.local>
2018-03-06 16:47:22 +0000

    initialize application

---
+++ services/users/project/config.py
@@ -0,0 +1,12 @@
+class BaseConfig:
+  """Base configuration"""
+  TESTING = False
+
+class DevelopmentConfig(BaseConfig):
+    pass
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+
+class ProductionConfig(BaseConfig):
+    pass
